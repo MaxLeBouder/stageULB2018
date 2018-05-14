efec999a753d5ea55bfc9894663a3fe832e7c112
MaxLeBouder <32568062+MaxLeBouder@users.noreply.github.com>
2018-05-14 14:50:35 +0200

    Update decompressor.cpp

--- decompressor.cpp
+++ decompressor.cpp
@@ -49,8 +49,6 @@ int main(int argc, char ** argv){
 		exit(1);
 	}
 
-	/*A MODIFIER POUR LA DECOMPRESSION => */
-
 	//DECOMPRESSION
 
 	//FILE OPEN
@@ -64,18 +62,19 @@ int main(int argc, char ** argv){
 	read_encoding read_prec;
 	int anchor_value;
 	int anchor_prec = 0;
-	int read_position;
+	int position;
 	int pos_prec = 0;
 
 	while(not in.eof()){
 
-
 		//Reading anchor absolute value
 		getline(in,parse,(char)255);
 		if(not parse.empty()){
-			anchor_value=str_bin_to_int(parse);	//A CORRIGER
+			//Retrieve anchor value and convert it to integer
+			anchor_value=str_bin_to_int(parse);	
 			if(DEBUG){cout<<parse<<"("<<anchor_value<<")";}
-			anchor_value = anchor_value + anchor_prec;
+			//The anchor value is equals to the previous value and the current relative position
+			anchor_value = abs(anchor_value) + abs(anchor_prec);
 			anchor_prec = anchor_value;
 		}
 
@@ -83,12 +82,45 @@ int main(int argc, char ** argv){
 
 		//Reading paths for the positive anchor
 		getline(in,parse,(char)255);
-		//If it's not empty
-		if(not parse.empty()){											//Ajouter parcours ancre+ TANT QUE chemin à lire
-			//if(DEBUG){cout<<"Ancre positive"<<endl;}
+		if(not parse.empty()){											
+			if(DEBUG){cout<<"Ancre positive"<<endl;}
 
 			//Reading path and positions
 			std::stringstream parse_s(parse);
+			char current;
+			while(not parse_s.eof()){
+				if(current == (char)254){
+					pos_prec = 0;
+					//Read the compressed path
+					getline(parse_s,path,':');
+					//Read the first position
+					parse_s.get(current);
+					if(current == '0'){
+						position = 0;
+					}else{
+						position = (unsigned char)current;
+					}
+					//Push the read with the first position
+					reads.push_back({anchor_value,position,path});
+
+					int next = parse_s.peek();
+					//If there's a multiple position sign
+					if(next == '#'){
+						pos_prec = position;
+						parse_s.get(current); //To discard the '#'
+					}
+				//If we're reading relative positions after a '#', then pos_prec is set
+				}else if(pos_prec > 0){
+					position = pos_prec + (unsigned char)current;
+					reads.push_back({anchor_value,position,path});
+					pos_prec = position;
+				}
+
+				//Get the next character in the parse stream
+				parse_s.get(current);
+			}
+
+			/*
 			getline(parse_s,parse_path,(char)254);
 
 
@@ -105,90 +137,88 @@ int main(int argc, char ** argv){
 
 			//If there is a single position (no multiple position marker)
 			if(pos_separator == string::npos){
-				read_position = parse_path[pos_begin+1];
-				reads.push_back({anchor_value,read_position,path});
+				position = parse_path[pos_begin+1];
+				if(DEBUG){cout<<" position:"<<position<<endl;}
+				reads.push_back({anchor_value,position,path});
 
 			//Else read all the positions one by one 
 			}else{
 				//First position
-				read_position = parse_path[pos_begin+1];
-				reads.push_back({anchor_value,read_position,path});
-				pos_prec = read_position;
+				position = parse_path[pos_begin+1];
+				reads.push_back({anchor_value,position,path});
+				pos_prec = position;
 
 				//Other positions, from after # to the end of parse_path
 				for(std::string::size_type i = pos_separator; i<parse_path.size(); ++i){
-					read_position = parse_path[i] + pos_prec;
-					reads.push_back({anchor_value,read_position,path});
-					pos_prec = read_position;
+					position = parse_path[i] + pos_prec;
+					reads.push_back({anchor_value,position,path});
+					pos_prec = position;
 				}
 			}
-
+			*/
 		}
-
 
 
 
 		//Reading paths for the negative anchor
 		getline(in,parse,(char)255);
-		if(not parse.empty()){											//Ajouter parcours ancre- TANT QUE chemin à lire
-			//if(DEBUG){cout<<"Ancre negative"<<endl;}
+		if(not parse.empty()){											
+			if(DEBUG){cout<<"Ancre negative"<<endl;}
 
-			//Anchor value is negative
+			//The anchor value is negative
 			anchor_value = -anchor_value;
 
 			//Reading path and positions
 			std::stringstream parse_s(parse);
-			getline(parse_s,parse_path,(char)254);
+			char current;
+			while(not parse_s.eof()){
+				//Get the next character in the parse stream
+				parse_s.get(current);
 
-			//There can be anchors without paths but with positions
-			//if(not parse_path.empty())
+				if(current == (char)254){
+					pos_prec = 0;
+					//Read the compressed path
+					getline(parse_s,path,':');
+					//Read the first position
+					parse_s.get(current);
+					if(current == '0'){
+						position = 0;
+					}else{
+						position = (unsigned char)current;
+					}
+					//Push the read with the first position
+					reads.push_back({anchor_value,position,path});
 
-			//Retrieve the path
-			std::stringstream path_s(parse_path);
-			getline(path_s,path,':');
-
-			std::size_t pos_begin = parse_path.find(":");
-			std::size_t pos_separator = parse_path.find("#"); 
-
-			//If there is a single position (no multiple position marker)
-			if(pos_separator == string::npos){
-
-				read_position = parse_path[pos_begin+1];
-				reads.push_back({anchor_value,read_position,path});
-			//Else read all the positions one by one 
-			}else{
-				//First position
-				read_position = parse_path[pos_begin+1];
-				reads.push_back({anchor_value,read_position,path});
-				pos_prec = read_position;
-
-				//Other positions, from after # to the end of parse_path
-				for(std::string::size_type i = pos_separator; i<parse_path.size(); ++i){
-					read_position = parse_path[i] + pos_prec;
-					reads.push_back({anchor_value,read_position,path});
-					pos_prec = read_position;
+					int next = parse_s.peek();
+					//If there's a multiple position sign
+					if(next == '#'){
+						pos_prec = position;
+						parse_s.get(current); //To discard the '#'
+					}
+				//If we're reading relative positions after a '#', then pos_prec is set
+				}else if(pos_prec > 0){
+					position = pos_prec + (unsigned char)current;
+					reads.push_back({anchor_value,position,path});
+					pos_prec = position;
 				}
 			}
-
 		}
 
 	}
-	//reads.push_back({anchor_value,read_position,path_direction});
-
 	//End of reading
 
 	//Writing the path on a file
 	ofstream stream_paths("decompressedPaths");
 
-	//if(DEBUG){cout<<endl<<"Ecriture"<<endl;}
+	if(DEBUG){cout<<endl<<"Ecriture"<<endl;}
 	for(uint i(0); i<reads.size(); ++i){
-		//if(DEBUG){cout<<i<<"/"<<reads.size()<<endl;}
+		if(DEBUG){cout<<i+1<<"/"<<reads.size()<<endl;}
 		stream_paths<<reads[i].anchor_number<<":";
 		if(not reads[i].path_direction.empty()){
 			stream_paths<<reads[i].path_direction;
 		}
 		stream_paths<<":"<<reads[i].read_position;
 
-		stream_paths<<endl;
+		stream_paths<<":"<<endl;
 	}
 }
